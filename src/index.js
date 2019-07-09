d645c8a75ac0190648d603b544b3449cb9444f17
Ran Reichman <ran.reichman@gmail.com>
2019-07-08 22:47:44 -0700

    updated dependancy

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import ImageCarousel from "../src/image_carousel/image_carousel.js";
 const Index = () => {
   return (
     <div className="main">
+        Profile
         <div className="profile">
             <ProfileImage image_path="../images/sunset-pexels.jpeg"/>
         </div>
+        Slideshow
         <div className="slideshow">
             <ImageCarousel/>
         </div>
