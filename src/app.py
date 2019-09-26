48289745567fe0c8da481f4664159de662535b86
Ran Reichman <ran.reichman@gmail.com>
2019-09-25 21:56:55 -0700

    now with flask for heroku experiment

--- src/app.py
+++ src/app.py
@@ -1,9 +1,20 @@
-import http.server
-import socketserver
+#import http.server
+#import socketserver
 
-PORT = 8080
-Handler = http.server.SimpleHTTPRequestHandler
+#PORT = 8080
+#Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+#with socketserver.TCPServer(("", PORT), Handler) as httpd:
+#    print("serving at port", PORT)
+#    httpd.serve_forever()
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from Python!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
