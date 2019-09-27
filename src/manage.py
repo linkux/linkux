16ce3fc1263648a6580ad560863a103bf1f59c03
Ran Reichman <ran.reichman@gmail.com>
2019-09-26 22:26:04 -0700

    trying to adapt to heroku

---
+++ src/manage.py
@@ -0,0 +1,10 @@
+import http.server
+import socketserver
+
+PORT = 8080
+Handler = http.server.SimpleHTTPRequestHandler
+
+if __name__ == '__main__':
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
