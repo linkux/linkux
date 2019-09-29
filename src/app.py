9dc28860ad7e996be205efa4f346722b3fe043e6
Ran Reichman <ran.reichman@gmail.com>
2019-09-29 15:45:52 -0700

    now using flask

--- src/app.py
+++ src/app.py
@@ -2,12 +2,23 @@
 
 import http.server
 import socketserver
-import sys
 
-PORT = 80
+cert_folder = "~/"
+
+from http.server import HTTPServer,SimpleHTTPRequestHandler,BaseHTTPRequestHandler
+import ssl
+
+if __name__ == '__main__':
+    PORT = 1443
+    httpd = HTTPServer(('localhost', PORT), SimpleHTTPRequestHandler)
+    httpd.socket = ssl.wrap_socket (httpd.socket, certfile='fullchain.pem', keyfile='privkey.pem', server_side=True)
+    print("serving at port", PORT)
+    httpd.serve_forever()
+
+'''PORT = 80
 Handler = http.server.SimpleHTTPRequestHandler
 
 if __name__ == '__main__':
     with socketserver.TCPServer(("", PORT), Handler) as httpd:
         print("serving at port", PORT)
-        httpd.serve_forever()
+        httpd.serve_forever()'''
