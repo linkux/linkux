159367ce352eaeaba8d651c17e09dc8b03d51c7a
Ran Reichman <ran.reichman@gmail.com>
2019-11-28 02:00:34 -0800

    minor styling bug fixes

--- src/static/load_list_data.js
+++ src/static/load_list_data.js
@@ -6,6 +6,12 @@ function makeUL(array) {
 
         item.appendChild(document.createTextNode(array[i]));
         //item.setAttribute("style","max-width:100px");
+        fontsize_string = "4vw";
+
+        if (screen.width > 421){
+            fontsize_string = "16px";
+        }
+        item.setAttribute("style","font-size:" + fontsize_string + ";margin-bottom:5px");
 
         list.appendChild(item);
     }
