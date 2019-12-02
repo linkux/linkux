a52dcd73f86908705099200d44e9be28cf94dd6f
Ran Reichman <root@ip-172-31-21-183.us-west-2.compute.internal>
2019-12-02 03:36:44 +0000

    removed test

--- src/node-server/index.js
+++ src/node-server/index.js
@@ -8,7 +8,6 @@ app.use(express.static(__dirname + "/../templates"));
 app.use(express.static(__dirname + "/../static"));
 app.use(express.static(__dirname + "/.."));
 
-# test
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/../templates/index.html'));
 });
