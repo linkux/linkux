b427750addb25cc0e5b79bb84f84b8d0a7184c1e
Ubuntu <ubuntu@ip-172-31-21-183.us-west-2.compute.internal>
2019-12-02 03:23:05 +0000

    now using node on backend

---
+++ src/node-server/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + "/../templates"));
+app.use(express.static(__dirname + "/../static"));
+app.use(express.static(__dirname + "/.."));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../templates/index.html'));
+});
+
+app.listen(3001);
