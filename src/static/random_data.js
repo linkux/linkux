0c2389cf0c3318249dcb97545240815e6b588be0
Ran Reichman <ran.reichman@gmail.com>
2019-12-04 21:17:25 -0800

    content

--- src/static/random_data.js
+++ src/static/random_data.js
@@ -1,6 +1,16 @@
 var random = [
     {
     "" :
+    "4/12/19 - Thinking about the GAFA companies. " +
+    "At $872bn, Amazon is way undervalued. My bet is that in 10 years, AWS and the logistics/retail " +
+    "business will each be worth more than that. At $911bn, Google is also undervalued but to a lesser extent. " +
+    "YouTube is so big that it alone should be at least double Netflix (currently $133bn). Waymo will be worth a lot." +
+    " And then they have Maps, Chrome, Android, Gmail, and of course Search. Apple is killing it in wearables and has " +
+    " a giant head start in AR. Probably won't double in 10 years but my bet is >$2 trillion, vs $1.18 trillion now." +
+    " Facebook will make a ton of money in the 2020s but I'd be surprised if it ever passed $1 trillion (now $567bn)."
+    },
+    {
+    "" :
     "4/12/19 - String beans with rice is always more satiating than you originally think."
     },
     {
