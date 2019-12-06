f6b58eace39aeebed7fbf0685d3f9367fca54016
Ran Reichman <ran.reichman@gmail.com>
2019-12-05 20:31:50 -0800

    content

--- src/static/books_data.js
+++ src/static/books_data.js
@@ -1,6 +1,7 @@
 var books_seperator = ", by ";
 
 var books = [
+    "Ride of a Lifetime;Bob Iger;**",
     "Kochland: The Secret History of Koch Industries;Christopher Leonard;***",
     "Private Empire: ExxonMobil and American Power;Steve Coll;**",
     "The Man Who Solved The Market;Gregory Zuckerman;***",
